b21c008e6e95a5e3ff1f1928d24302b94cdfccb8
Deepanshu Jain <152912677+Deepanshu28071998@users.noreply.github.com>
2024-08-09 22:24:40 +0530

    Add files via upload

---
+++ Find_secondLargest_element_in_Array.java
@@ -0,0 +1,36 @@
+package Array;
+
+class SecondLargest
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 6, 3, 9, 10, 7, 4};
+        int n = arr.length;
+        int max = 0;
+        int secondMax = 0;
+
+        if (arr[0] >= arr[1])
+        {
+            max = arr[0];
+            secondMax = arr[1];
+        }
+        else
+        {
+            max = arr[1];
+            secondMax = arr[0];
+        }
+        for (int i = 2; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > secondMax && arr[i] != max)
+            {
+                secondMax = arr[i];
+            }
+        }
+        System.out.println("Second largest element is " + secondMax);
+    }
+}
