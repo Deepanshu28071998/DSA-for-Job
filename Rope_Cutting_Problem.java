01f0fb877ec5ba59be2b8b490bbf8ae08444537f
Deepanshu Jain <152912677+Deepanshu28071998@users.noreply.github.com>
2024-09-10 19:53:06 +0000

    Rope cutting Problem in java using Recursion Method

---
+++ Rope_Cutting_Problem.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Rope_Cutting_Problem
+{
+    public static int maxPieces(int n, int a, int b, int c)
+    {
+            
+            if (n == 0) return 0;
+            if (n < 0) return -1;
+            int res = Math.max(maxPieces(n - a, a, b, c), Math.max(maxPieces(n - b, a, b, c), maxPieces(n - c, a, b, c)));
+
+            if (res == -1) return -1;
+            return res + 1;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            
+            int a = sc.nextInt();
+
+            int b = sc.nextInt();
+
+            int c = sc.nextInt();
+
+            int res = maxPieces(n, a, b, c);
+            System.out.println(res);
+
+            // Scanner.close();
+    }
+}
