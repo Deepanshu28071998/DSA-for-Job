9cb8eb2dd45882e53b51aa0e96afece9ae46412e
Deepanshu Jain <152912677+Deepanshu28071998@users.noreply.github.com>
2024-09-11 08:10:57 +0000

    Tower of Hanoi problem using Recursion method in Python

--- Tower_of_Hanoi.py
+++ Tower_of_Hanoi.py
@@ -1,14 +1,14 @@
-def TOH(n, A, B,C):
+def TOH(n, A, B, C):
     if (n == 1):
-        print("Move 1 from "+A+" to "+C)
+        print("Move 1 from",A,"to",C)
         return
     
     TOH(n - 1, A, C, B)
-    print("Move "+n+" from "+A+" to "+C)
-    TOH(n -1, B, A, C)
+    print("Move",n,"from",A,"to",C)
+    TOH(n -1, C, B, A)
 
 def main():
     n = int(input())
-    TOH(n, 'A', 'B', 'C')
+    TOH(n, 'A', 'C', 'B')
 
 main()
